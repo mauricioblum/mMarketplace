c9b56b950c1daf08d0e2a0fa1ad3f5c59f9de2fc
Mauricio Blum <mauriciocblum@gmail.com>
2019-02-26 02:00:01 -0300

    Added  Password bcrypt hashing

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,24 +1,33 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: true
   },
   email: {
     type: String,
     require: true,
     unique: true,
-    lowercase: true,
+    lowercase: true
   },
-  password {
+  password: {
     type: String,
-    required: true,
+    required: true
   },
-  createdAt:{
+  createdAt: {
     type: Date,
     default: Date.now
   }
+})
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next()
+  }
+
+  this.password = await bcrypt.hash(this.password, 8)
 })
 
 module.exports = mongoose.model('User', UserSchema)
